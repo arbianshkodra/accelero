e06522d47ba0730e5bce8db51f8967cbd9db1ca7
Arbian Shkodra <arbianshkodra@gmail.com>
2024-10-20 15:32:14 +0200

    chore: Update Docker and Go dependencies, and use container and list options from the Docker API package

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -14,7 +15,7 @@ type MockClient struct {
 	mock.Mock
 }
 
-func (m *MockClient) ContainerList(ctx context.Context, options types.ContainerListOptions) ([]types.Container, error) {
+func (m *MockClient) ContainerList(ctx context.Context, options container.ListOptions) ([]types.Container, error) {
 	args := m.Called(ctx, options)
 	return args.Get(0).([]types.Container), args.Error(1)
 }
